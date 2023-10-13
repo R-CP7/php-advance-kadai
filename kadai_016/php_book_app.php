d0d542842fc2d0860e5fd42e32a0fb1312bae7e0
R-CP7 <138663910+R-CP7@users.noreply.github.com>
2023-10-13 18:08:48 +0900

    Add files via upload

---
+++ kadai_016/php_book_app.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <title>書籍管理アプリ</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="http://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+  </head>
+
+<body>
+  <header>
+    <a href="php_book_app.php">籍管理アプリ</a>
+</header>
+<main>
+  <article class="home">
+    <h1>籍管理アプリ</h1>
+    <p>『PHPとデータベースを連携しよう』成果物</p>
+    <a href= "read.php" class="btn">書籍一覧</a>
+</article>
+</main>
+<footer>
+  <p class="copyright">&copy;籍管理アプリ All rights reserved.</p>
+</footer>
+</body>
+</html>
